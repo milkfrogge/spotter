7e4ff526e44888dbf6cc978b2382b7d1153532b8
Nikita <39128285+milkfrogge@users.noreply.github.com>
2023-01-27 16:40:30 +0300

    begin to write service.go

--- src/internal/user/db/postgresql.go
+++ src/internal/user/db/postgresql.go
@@ -8,40 +8,40 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type storage struct {
+type UserStorage struct {
 	client client.Client
 	log    *logrus.Logger
 }
 
-func (s *storage) CreateByEmail(ctx context.Context, user user.CreateByEmailDTO) {
+func (s *UserStorage) CreateByEmail(ctx context.Context, user user.CreateByEmailDTO) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s *storage) CreateByPhoneNumber(ctx context.Context, user user.CreateByPhoneNumberDTO) {
+func (s *UserStorage) CreateByPhoneNumber(ctx context.Context, user user.CreateByPhoneNumberDTO) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s *storage) FindOne(ctx context.Context, id int) {
+func (s *UserStorage) FindOne(ctx context.Context, id int) {
 	//TODO implement me
 	var name, email, hash string
 	_ = s.client.QueryRow(ctx, "select name, email, password_hash from public.user where id=1").Scan(&name, &email, &hash)
 	fmt.Printf("%s, %s, %s\n", name, email, hash)
 }
 
-func (s *storage) Update(ctx context.Context, user user.User) {
+func (s *UserStorage) Update(ctx context.Context, user user.User) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s *storage) Delete(ctx context.Context, id int) {
+func (s *UserStorage) Delete(ctx context.Context, id int) {
 	//TODO implement me
 	panic("implement me")
 }
 
 func NewStorage(client client.Client, log *logrus.Logger) user.Storage {
-	return &storage{
+	return &UserStorage{
 		client: client,
 		log:    log,
 	}
